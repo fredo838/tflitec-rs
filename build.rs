772c08759319ec1af78346a8ffb962f0e73a30cf
Frederik Bode <fredo.bode@gmail.com>
2023-10-17 12:52:34 +0200

    update

--- build.rs
+++ build.rs
@@ -521,8 +521,8 @@ fn main() {
     if os != "ios" {
         println!("cargo:rustc-link-search=native={}", out_path.display());
         println!("cargo:rustc-link-lib=dylib=tensorflowlite_c");
+        println!("cargo:rustc-link-search=native={}", out_path.display());
         println!("cargo:rustc-link-lib=dylib=tensorflowlite_flex");
-        println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
     } else {
         println!("cargo:rustc-link-search=framework={}", out_path.display());
         println!("cargo:rustc-link-lib=framework=TensorFlowLiteC");
